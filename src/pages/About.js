552d251132f55ff2d2746ccf9aec7284e276877d
Chims <chimajunior14@gmail.com>
2023-09-18 18:39:26 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MultiplePizza from "../assets/multiplePizzas.jpeg";
+import "../styles/About.css";
+
+function About() {
+  return (
+    <div className="about">
+      
+     
+    </div>
+  );
+}
+
+export default About;
