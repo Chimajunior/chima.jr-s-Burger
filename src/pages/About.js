32dd801d2fb8b545858fd1aa672705ea0392ac30
Chims <chimajunior14@gmail.com>
2023-09-18 18:50:13 +0100

    added bg image

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,10 @@ import "../styles/About.css";
 function About() {
   return (
     <div className="about">
-      
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      ></div>
       <div className="aboutBottom">
         
       </div>
