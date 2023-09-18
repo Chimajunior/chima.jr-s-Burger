771aac2dcc16b57cd88b39bacfa5c8fcf04fa896
Chims <chimajunior14@gmail.com>
2023-09-18 19:00:33 +0100

    mapping and adding props to the menu component

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -7,7 +7,18 @@ function Menu() {
   return (
     <div className="menu">
       <h1 className="menuTitle">Our Menu</h1>
-      
+      <div className="menuList">
+        {MenuList.map((menuItem, key) => {
+          return (
+            <MenuItem
+            key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
