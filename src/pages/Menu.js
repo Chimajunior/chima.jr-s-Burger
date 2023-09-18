f4c50d0ca8f74c4c16c6ed07049188ac11bc6533
Chims <chimajunior14@gmail.com>
2023-09-18 18:59:31 +0100

    initial comm

---
+++ src/pages/Menu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MenuList } from "../helpers/MenuLists";
+import MenuItem from "../components/MenuItem";
+import "../styles/Menu.css"
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      
+    </div>
+  );
+}
+
+export default Menu;
