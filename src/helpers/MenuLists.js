1554e696f9484ee8307aae633efa7ee99b012178
Chims <chimajunior14@gmail.com>
2023-09-18 18:58:12 +0100

    contents assigned to the props

---
+++ src/helpers/MenuLists.js
@@ -0,0 +1,44 @@
+import Regular from "../assets/burgerPlain.jpg";
+import fries from "../assets/bandFries.jpg";
+import ChimaSpecial from "../assets/chimaspecial.jpg";
+import Decker from "../assets/deckerBurg.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Regular Burger",
+    image: Regular,
+    price: 4500,
+  },
+  {
+    name: "Burgers and Fries",
+    image: fries,
+    price: 8500,
+  },
+  {
+    name: "Decker Burger",
+    image: Decker,
+    price: 8000,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 6500,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 6500,
+  },
+  {
+    name: "Chima.jr Special Pizza",
+    image: ChimaSpecial,
+    price: 15000,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 20000,
+  },
+];
