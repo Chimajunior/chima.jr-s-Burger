4af64e0f142aa4e0d9fe72726d3de06164bbed76
Chims <chimajunior14@gmail.com>
2023-09-18 18:33:26 +0100

    initial commit

---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PizzaLeft from "../assets/pizzaLeft.jpg";
+import "../styles/Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+
+      ></div>
+      <div className="rightSide">
+        
+   
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
