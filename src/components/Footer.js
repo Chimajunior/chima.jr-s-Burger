ead96f91a2ecf05939efc3d4d8fd651fb4f0e3d2
Chims <chimajunior14@gmail.com>
2023-09-29 19:26:11 +0100

    changed some footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
         <FacebookIcon />
         <LinkedInIcon />
       </div>
-      <p>&copy; 2023 chimajrpizza</p>
+      <p>&copy; 2023 chimajrdiner</p>
     </div>
   );
 }
