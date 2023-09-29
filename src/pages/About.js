9d13bfcaa1ae61a2f21888e0c769ede9ea27caff
Chims <chimajunior14@gmail.com>
2023-09-29 19:26:45 +0100

    added contents to the p tag

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,20 +11,23 @@ function About() {
       ></div>
       <div className="aboutBottom">
       <h1> ABOUT US</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+      <p>
+      
+
+<h2>Welcome to Chima Jr's.!</h2><br/>
+
+Chima Jr's Diner. was founded in 2021 with a clear vision: to serve quality foods that empower families and individuals to thrive in the digital age. <br/><br/>
+
+<h2>Our Mission</h2>
+
+At Chima Jr's Diner., our mission is simple yet profound: to harness the power of food to solve real-world hunger and drive positive change. We are committed to delivering quality food that transform the way you eat, feel, and grow.<br/>
+
+<h2>What Sets Us Apart</h2>
+
+What sets Chima Jr's. apart is our relentless pursuit of excellence. We believe in staying ahead of the curve, and our team of talented chefs and waiters ensures that we remain at the forefront of resturants. We take pride in our ability to adapt, evolve, and deliver results.
+
+
+
         </p>
       </div>
     </div>
