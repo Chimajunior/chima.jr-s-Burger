7afd6ae974fb91f86d85a879ff32bdf4a95422fa
Chims <chimajunior14@gmail.com>
2023-09-29 19:25:35 +0100

    changed some contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/pizza.jpeg"
+import BannerImage from "../assets/burgerHome.jpg"
 import "../styles/Home.css"
 
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})`}}>
       <div className="headerContainer">
-        <h1>Chima jr's Pizza</h1>
-        <p>PIZZA TO FIT ANY TASTE</p>
+        <h1>Chima jr's Diner</h1>
+        <p>PIZZA AND BURGER TO FIT ANY TASTE</p>
         <Link to="/menu" >
         <button>ORDER NOW</button>
         </Link>
